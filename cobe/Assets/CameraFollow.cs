26fd2c2ed4ff1e3973f906ca01c355d736338219
PhillyTheChilly <117826881+PhillyTheChilly@users.noreply.github.com>
2022-12-21 21:39:42 +0100

    The cock and finished update ( kinda)
    8=====================D

--- cobe/Assets/CameraFollow.cs
+++ cobe/Assets/CameraFollow.cs
@@ -4,28 +4,34 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
-    public float smoothSpeed = 0.2f;
-    public Vector3 offset;
+    public Transform lookAt;
+    public Transform camTransform;
+
+    private Camera cam;
+
+    private float distance = 10.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    private float sensitivityX = 4.0f;
+    private float sensitivityY = 1.0f;
+
+    private void Update()
+    {
+        currentX += Input.GetAxis("Mouse X");
+        currentY += Input.GetAxis("Mouse Z");
+    }
+
+    private void Start()
+    {
+        camTransform = transform;
+        cam = Camera.main;
+    }
 
     private void LateUpdate()
     {
-        transform.position = target.position + offset;
-
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
+        Vector3 dir = new Vector3(0, 0,-distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        camTransform.position = lookAt.position + rotation * dir;
+        camTransform.LookAt(lookAt.position);
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+}   
